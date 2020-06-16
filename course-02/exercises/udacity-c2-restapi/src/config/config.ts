fb1f3299877747954d1a65a47624a730a8634f56
mmahlies <eng.m.mahlies@gmail.com>
2020-06-17 00:51:20 +0200

    building and deploy

---
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,27 @@
+export const config = {
+  "dev": {
+
+
+// "username": "postgres",
+// "password": "database-1",
+// "database": "udagram",
+// "host": "database-1.cjqzv1eedxia.eu-central-1.rds.amazonaws.com",
+    "username": process.env.udagram_username,
+    "password": process.env.udagram_password,
+    "database": process.env.udagram_database,
+    "host": process.env.udagram_host,
+    "dialect": "postgres",
+    "aws_region": "eu-central-1",
+    "aws_profile": "DEPLOYED",
+    "aws_media_bucket": "udagram-talaat-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
+
+
