bf450ba0417d388f40ad397cb0926c19740768aa
mmahlies <eng.m.mahlies@gmail.com>
2020-06-18 23:17:48 +0200

    test commit

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -5,7 +5,7 @@ import * as AWS from '../../../../aws';
 
 const router: Router = Router();
 
-// Get all feed items
+/// Get all feed items
 router.get('/', async (req: Request, res: Response) => {
     const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
     items.rows.map((item) => {
